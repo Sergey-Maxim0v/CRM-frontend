148d7029585041d5b7dda825ccbb614d7fd86020
Sergey Maximov <dev9092336062@gmail.com>
2023-05-19 16:07:39 +0300

    tsconfig:  never read imports and variables, .prettierignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
+    plugins: [react()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `
                                     @import "src/assets/styles/constants.scss";
                                 `,
-      },
+            },
+        },
     },
-  },
 });
