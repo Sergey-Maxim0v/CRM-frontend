df881256924101def8e32b5664cd666116ba37c9
Sergey Maximov <dev9092336062@gmail.com>
2023-05-30 14:34:42 +0300

    contacts href

--- src/components/contact-component/index.tsx
+++ src/components/contact-component/index.tsx
@@ -3,13 +3,37 @@ import { IContactComponent } from "./types";
 import styles from "./styles.module.scss";
 import ComponentsSVGContacts from "../components-svg-contacts";
 import classNames from "classnames";
+import { CONTACT_TYPES } from "../../api/types";
 
 const ContactComponent: FC<IContactComponent> = ({ contact, className }) => {
-  const href = "/";
-  // TODO: href
+  const getHref = () => {
+    switch (contact.type) {
+      case CONTACT_TYPES.tel:
+        return `tel:${contact.value}`;
+      case CONTACT_TYPES.otherTel:
+        return `tel:${contact.value}`;
+      case CONTACT_TYPES.email:
+        return `mailto:${contact.value}`;
+      case CONTACT_TYPES.telegram:
+        return `https://telegram.me/${contact.value}`;
+      case CONTACT_TYPES.vk:
+        return `https://vk.com/${contact.value}`;
+      default:
+        return "/";
+    }
+  };
 
   return (
-    <a href={href} className={classNames(styles.link, className)}>
+    <a
+      href={getHref()}
+      target={
+        contact.type === CONTACT_TYPES.vk ||
+        contact.type === CONTACT_TYPES.telegram
+          ? "_blank"
+          : undefined
+      }
+      className={classNames(styles.link, className)}
+    >
       <ComponentsSVGContacts type={contact.type} className={styles.icon} />
     </a>
   );
